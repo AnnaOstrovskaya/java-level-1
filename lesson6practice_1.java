ecf30c58b8cd30b9432c7624fb5a15cd381831c6
AnnaOstrovskaya <85437758+AnnaOstrovskaya@users.noreply.github.com>
2021-12-30 12:44:41 +0000

    Update lesson6practice_1.java

--- lesson6practice_1.java
+++ lesson6practice_1.java
@@ -3,6 +3,7 @@
  * <p>
  * stuent: Anna Ostrovskaya
  * version 1: 25.12.2021
+ * version 2: 30.12.2021
  */
 
 interface IAnimal {
@@ -15,8 +16,8 @@ abstract class Animal implements IAnimal {
 
     protected String name;
 
-    protected int MAX_RUN_DISTANCE;
-    protected int MAX_SWIM_DISTANCE;
+    protected int max_run_distance;
+    protected int max_swim_distance;
 
     abstract public String run(int distance);
 
@@ -32,13 +33,13 @@ class Cat extends Animal {
 
     public Cat(String name) {
         super(name);
-        this.MAX_RUN_DISTANCE = 200;
-        this.MAX_SWIM_DISTANCE = 0;
+        this.max_run_distance = 200;
+        this.max_swim_distance = 0;
     }
 
     @Override
     public String run(int distance) {
-        if (distance <= this.MAX_RUN_DISTANCE) {
+        if (distance <= this.max_run_distance) {
             return this.name + " ran " + Integer.toString(distance) + " meters";
         } else {
             return this.name + " CAN NOT run " + Integer.toString(distance) + " meters";
@@ -49,20 +50,19 @@ class Cat extends Animal {
     public String swim(int distance) {
         return this.name + " is a cat. Cat's can't swim!";
     }
-
 }
 
 class Dog extends Animal {
 
     public Dog(String name) {
         super(name);
-        this.MAX_RUN_DISTANCE = 500;
-        this.MAX_SWIM_DISTANCE = 10;
+        this.max_run_distance = 500;
+        this.max_swim_distance = 10;
     }
 
     @Override
     public String run(int distance) {
-        if (distance <= this.MAX_RUN_DISTANCE) {
+        if (distance <= this.max_run_distance) {
             return this.name + " ran " + Integer.toString(distance) + " meters";
         } else {
             return this.name + " CAN NOT run " + Integer.toString(distance) + " meters";
@@ -71,7 +71,7 @@ class Dog extends Animal {
 
     @Override
     public String swim(int distance) {
-        if (distance <= this.MAX_SWIM_DISTANCE) {
+        if (distance <= this.max_swim_distance) {
             return this.name + " swam " + Integer.toString(distance) + " meters";
         } else {
             return this.name + " CAN NOT swim " + Integer.toString(distance) + " meters";
@@ -84,7 +84,18 @@ public class lesson6practice_1 {
 
     public static void main(String[] args) {
 
+        // my way
         IAnimal cat = new Cat("Moorzik");
+
+        //proposed way
+        //IAnimal cat = new Cat("Muhtar", max_swim_distance=10, max_run_distance=500);
+        //IAnimal dog = new Dog("Moorzik", max_swim_distance=0, max_run_distance=100);
+
+        // drawbacks:
+        // 1. we are opening the doors to consumers of our class creating cat-like dogs and dog-like cats,
+        // making the class name (definition) meaningless and confusing
+        // 2. Have to define values in constructor each time (unless we specify default values)
+
         System.out.println(cat.run(10));
         System.out.println(cat.run(1000));
         System.out.println(cat.swim(1));
